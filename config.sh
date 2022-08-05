50751eaceac46d5163806bd75e8ff2e9466754c6
RandomPrototypes <delmotte_ar@yahoo.fr>
2022-08-06 01:29:04 +0900

    update onnxruntime version

--- config.sh
+++ config.sh
@@ -11,6 +11,6 @@ else
 	BufferedSocket_branch="v1.0.0"
 	RPCameraInterface_branch="v1.0.0"
 fi
-onnxruntime_branch="v1.10.0"
-onnxruntime_precompiled_archive="https://github.com/microsoft/onnxruntime/releases/download/v1.10.0/onnxruntime-linux-x64-gpu-1.10.0.tgz"
-onnxruntime_precompiled_folder="onnxruntime-linux-x64-gpu-1.10.0"
+onnxruntime_branch="v1.12.1"
+onnxruntime_precompiled_archive="https://github.com/microsoft/onnxruntime/releases/download/v1.12.1/onnxruntime-linux-x64-gpu-1.12.1.tgz"
+onnxruntime_precompiled_folder="onnxruntime-linux-x64-gpu-1.12.1"
